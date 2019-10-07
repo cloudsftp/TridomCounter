6713fabde8b071e9a8233aa815cff89be78d0907
Fabian Weik <fabian.weik@protonmail.com>
2019-10-07 15:26:05 +0200

    small changes layout etc

--- app/src/androidTest/java/de/melon/tridomcounter/activities/round/RoundActivityTest.kt
+++ app/src/androidTest/java/de/melon/tridomcounter/activities/round/RoundActivityTest.kt
@@ -1,15 +1,17 @@
 package de.melon.tridomcounter.activities.round
 
 import android.content.Intent
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import de.melon.tridomcounter.R
 import de.melon.tridomcounter.activities.current
 import de.melon.tridomcounter.data.GameData
-import org.junit.After
-import org.junit.Before
-import org.junit.FixMethodOrder
-import org.junit.Rule
+import org.junit.*
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
 
@@ -27,21 +29,26 @@ class RoundActivityTest {
     }
 
     val players = arrayOf("Fabian", "Finn", "Kenji")
+    val firstPlayerId = 0
 
     fun createSession() {
         current.sessionId = GameData.newSession(players)
         val session = GameData.sessions[current.sessionId]
-        current.roundId = session.newRound(0)
+        current.roundId = session.newRound(firstPlayerId)
 
     }
 
     @JvmField
     @Rule
-    val activityRule = ActivityTestRule<RoundActivity>(RoundActivity::class.java)
+    val activityRule = ActivityTestRule<RoundActivity>(RoundActivity::class.java, false, false)
 
     fun startActivity() = activityRule.launchActivity(Intent())
 
+    @Test
+    fun t00_activePlayerDisplayed() {
+        onView(withId(R.id.activePlayerNameTextView)).check(matches(withText(players[firstPlayerId])))
 
+    }
 
     @After fun tearDownIntents() = Intents.release()
 
--- app/src/main/java/de/melon/tridomcounter/activities/round/RoundActivity.kt
+++ app/src/main/java/de/melon/tridomcounter/activities/round/RoundActivity.kt
@@ -1,24 +1,47 @@
 package de.melon.tridomcounter.activities.round
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
 import de.melon.tridomcounter.R
-
+import de.melon.tridomcounter.activities.current
+import de.melon.tridomcounter.data.GameData
+import de.melon.tridomcounter.logic.Round
+import de.melon.tridomcounter.logic.Session
 import kotlinx.android.synthetic.main.activity_round.*
 
 class RoundActivity : AppCompatActivity() {
+
+    lateinit var session: Session
+    lateinit var round: Round
+
+    lateinit var activePlayerNameTextView: TextView
+    lateinit var playerActionsRecyclerView: RecyclerView
+    lateinit var actionsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_round)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        session = GameData.sessions[current.sessionId]
+        round = session.rounds[current.roundId]
+
+        activePlayerNameTextView = findViewById(R.id.activePlayerNameTextView)
+
+        playerActionsRecyclerView = findViewById(R.id.playerActionRecyclerView)
+        actionsRecyclerView = findViewById(R.id.actionRecyclerView)
+
+        updateActivePlayer()
+
+    }
+
+
+
+    fun updateActivePlayer() {
+        activePlayerNameTextView.text = session.players[round.playerId]
+
     }
 
     override fun onBackPressed() {}
