0145f96fb71a8ff7750948ff3c5d1e09a66acc0b
Fabian Weik <fabian.weik@protonmail.com>
2019-12-23 18:51:49 +0100

    even better solution

--- app/src/main/java/de/melon/tridomcounter/activities/round/EditTextExtension.kt
+++ app/src/main/java/de/melon/tridomcounter/activities/round/EditTextExtension.kt
@@ -5,7 +5,7 @@ import android.widget.EditText
 
 fun EditText.onSubmit(func: () -> Unit) = setOnEditorActionListener {
         _, actionId, _ ->
-        if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_NEXT)
+        if (actionId == EditorInfo.IME_ACTION_DONE)
             func()
 
         true
